8ec964e848deb50c4f7180a334acc77beb45eda1
Ohad Tal <89091610+OhadTal25@users.noreply.github.com>
2024-03-03 11:13:02 +0200

    Update MovieList.js

--- src/Pages/MovieList.js
+++ src/Pages/MovieList.js
@@ -1,14 +1,17 @@
-import React from "react";
-import { useState, useEffect } from "react";
+//Import necessary React hooks and components
+import { React, useState, useEffect } from "react";
 import { fetchMovieList } from "../Services/api";
 import MovieCard from "../Components/MovieCard";
 import "../Style/MovieList.css";
 
+// Defines the MovieList functional component
 const MovieList = () => {
+  //Initializes state variables for the movie list and search input
   const [list, setList] = useState([]);
-  const [searchItem, setSerachItem] = useState("");
+  const [searchItem, setSearchItem] = useState("");
 
   useEffect(() => {
+    //Asynchronously fetches movie data and sorts it by release date
     const getMovieList = async () => {
       const data = await fetchMovieList();
       data.sort((a, b) => new Date(b.release_date) - new Date(a.release_date));
@@ -17,10 +20,12 @@ const MovieList = () => {
     getMovieList();
   }, []);
 
+  //Updates the search input state when the user types in the search box
   const handleSearchChange = (event) => {
-    setSerachItem(event.target.value);
+    setSearchItem(event.target.value);
   };
 
+  //Filters the movie list based on the user's search input
   const filteredList = list.filter((movie) =>
     movie.title.toLowerCase().includes(searchItem.toLowerCase())
   );
@@ -31,11 +36,12 @@ const MovieList = () => {
         <input
           className="search-input"
           type="text"
-          placeholder="Search by movie name:"
+          placeholder="Search movie name:🍿"
           value={searchItem}
           onChange={handleSearchChange}
         />
       </div>
+      {/* Maps over the filtered list of movies to render individual MovieCard components */}
       <div className="list">
         {filteredList.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
