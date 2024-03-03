245b80d85d4fd2e6a58c5fcc00e10a6f0dad686c
Ohad Tal <89091610+OhadTal25@users.noreply.github.com>
2024-03-03 11:13:36 +0200

    Update api.js

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,8 +1,10 @@
+//import axios to handle GET requests
 import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3/movie";
 
+//fetchMovieList will fetch the GET request for the movie list
 export const fetchMovieList = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/popular?api_key=${API_KEY}`);
@@ -14,6 +16,7 @@ export const fetchMovieList = async () => {
   }
 };
 
+//fetchMovieDetails will fetch the GET request for the movie details
 export const fetchMovieDetails = async (id) => {
   try {
     const responseDetails = await axios.get(
