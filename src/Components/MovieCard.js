4191fccc29dc528bb741ab7239a335c09deda324
Ohad Tal <89091610+OhadTal25@users.noreply.github.com>
2024-03-03 11:14:56 +0200

    Update MovieCard.js

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,20 +1,24 @@
+// Import necessary elements from React, React Router, and local files
 import React from "react";
 import { Link } from "react-router-dom";
 import MovieAttribute from "./MovieAttribute";
+import "../Style/MovieList.css";
+import Poster from "./Poster";
 
+// MovieCard functional component to display each movie's card
 const MovieCard = ({ movie }) => (
   <div className="item">
+    {/* Link component from React Router to navigate to the movie's detail page */}
     <Link to={`/movie/${movie.id}`}>
-      <img
-        className="img"
-        src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
-        alt={movie.title}
-      />
+      {/* Poster component displaying the movie's poster */}
+      <Poster movie={movie} />
       <br />
       {movie.title}
     </Link>
+    {/* MovieAttribute component displaying the movie's rank */}
     <MovieAttribute title="Rank" value={movie.vote_average.toFixed(1)} />
   </div>
 );
 
+// Export the MovieCard component for use in other parts of the application
 export default MovieCard;
